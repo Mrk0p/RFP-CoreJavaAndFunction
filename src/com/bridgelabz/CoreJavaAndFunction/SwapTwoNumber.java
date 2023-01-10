eb2bebf56ffb5c3cd24b1c98eeca73632e26a145
Sachin wele <115727568+Mrk0p@users.noreply.github.com>
2023-01-10 10:12:48 +0530

    Added : SwapTwoNumber

--- src/com/bridgelabz/CoreJavaAndFunction/SwapTwoNumber.java
+++ src/com/bridgelabz/CoreJavaAndFunction/SwapTwoNumber.java
@@ -3,6 +3,7 @@ package com.bridgelabz.CoreJavaAndFunction;
 import java.util.Scanner;
 
 public class SwapTwoNumber {
+    
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
