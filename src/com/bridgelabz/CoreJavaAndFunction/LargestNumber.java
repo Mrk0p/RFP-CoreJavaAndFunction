34b679dd291ac87fc56325d1d96109a016ffe360
Sachin wele <115727568+Mrk0p@users.noreply.github.com>
2023-01-10 10:10:58 +0530

    Added :  LargestNumber

--- src/com/bridgelabz/CoreJavaAndFunction/LargestNumber.java
+++ src/com/bridgelabz/CoreJavaAndFunction/LargestNumber.java
@@ -3,6 +3,7 @@ package com.bridgelabz.CoreJavaAndFunction;
 import java.util.Scanner;
 
 public class LargestNumber {
+    
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
