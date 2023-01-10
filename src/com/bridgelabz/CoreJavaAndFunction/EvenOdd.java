631c9c9b19da398e42f7d3db8fb86460906c9f50
Sachin wele <115727568+Mrk0p@users.noreply.github.com>
2023-01-10 10:01:45 +0530

    Added :  EvenOdd

--- src/com/bridgelabz/CoreJavaAndFunction/EvenOdd.java
+++ src/com/bridgelabz/CoreJavaAndFunction/EvenOdd.java
@@ -3,6 +3,7 @@ package com.bridgelabz.CoreJavaAndFunction;
 import java.util.Scanner;
 
 public class EvenOdd {
+    
     public static void main(String[] args) {
 
         System.out.println("Enter a number:");
